562581a4f9b200d6a234fdbf9fd1a06d6993e614
See Xiong <seeseexiong@gmail.com>
2018-09-27 21:29:10 -0500

    3rd push

--- assests/javascript/main.js
+++ assests/javascript/main.js
@@ -1,20 +1,16 @@
-let correct = 0;
+let correctAnswer = 0;
 let incorrect = 0;
 let unanswered = 0;
 
-
-
-
 //once DOM is open, we should see only #container1.  Hide all other containers
-window.onload = function() {
-   
+$(document).ready(function(){
     $("#container2").hide();
     $("#container3").hide();
     
 //The game is time-limited to 120 seconds. This "TIME-LIMITED" will be set to count down which will be 
 //changing values turn TIME-LIMITED into a variable
-var timeLimited = 120;    
-var intervalId;
+let timeLimited = 3;    
+let intervalId;
 
 //when the button is click hide #container2 & #container#3 and show l#container1. Timer starts counting down - 
 //Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
@@ -22,29 +18,14 @@ var intervalId;
         $("#container1").hide();
         $("#container2").show();
         $("#container3").hide();
+        
         intervalId = setInterval(decrement, 1000);
     })
-        
-
+    
 
 
 //ALL THE FUNCTIONS STARTS HERE =========================================================================
-
-        //a function to ADD to CORRECT answers
-        function correctSum() {
-            cAnswers++;
-        }
-
-        //a function to ADD to INCORRECT answers
-        function incorrectSum() {
-            iAnswers++;
-        }
-
-        //a function to ADD to UNANSWERED questions
-        function unansweredSum() {
-            uAnswers++;
-        }
-
+    
         //a DECREMENT function to countdown
         function decrement() {
             timeLimited--;
@@ -52,6 +33,21 @@ var intervalId;
             if (timeLimited === 0) {
                 //...run the stop function.
                 stop();
+                
+                //after time is up, the document checks all the answers
+                let question1 = document.quiz.question1.value;
+                if (question1 === "Correct"){
+                    correctAnswer=+1;
+                    alert("correct");
+                }
+                else if (question1 === "Wrong"){
+                    alert("Wrong");
+                    incorrect++;
+                }
+                else {
+                    unanswered++;
+                }
+
                 //show the result(contained in #container3).  Hide all other containers.
                 $("#container1").hide();
                 $("#container2").hide();
@@ -59,12 +55,17 @@ var intervalId;
             }
         }//<--- closes the decrement function
 
-
         //a STOP funciton to stop the timer
         function stop() {
             clearInterval(intervalId);
         }
-} //<--- closes the window.onload function()
+
+        
+        //output the scores to DOM
+        $("#cAnswers").text("You got: " + correctAnswer );
+        $("#iAnswers").text("Wrong: " + incorrect );
+        $("#uAnswers").text("Unanswered: " + unanswered );
+}) //<--- closes the $(document).ready function()
 
 //when time is up, all answer is submit to server side 
 
